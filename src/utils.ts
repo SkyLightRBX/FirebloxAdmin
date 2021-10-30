5c62636b825e365786f0541f5b18468a64b5b286
piano1029 <46605242+piano1029@users.noreply.github.com>
2021-10-30 14:11:26 +0200

    Add base urls in utils.ts

---
+++ src/utils.ts
@@ -0,0 +1,5 @@
+export const FIRESTORE_BASE_ADDRESS = "https://firestore.googleapis.com/v1/"
+
+export function FIRESTORE_DATABASE_ADDRESS(ProjectId:string, DatabaseID:string = "default") {
+    return `${FIRESTORE_BASE_ADDRESS}projects/${ProjectId}/databases/${DatabaseID}/`
+}
